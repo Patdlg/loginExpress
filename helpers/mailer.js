ab57fe3f7c7f5eee9e25bb14de26fcb2239b0f16
Patrick de la Guardia <patrickdelaguardia@Patricks-MacBook.local>
2018-07-12 12:25:04 -0500

    fixed

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -3,37 +3,37 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: process.env.gmail,
-      pass: process.env.gmailPass
-    }
-  });
+  service: 'Gmail',
+  auth: {
+    user: process.env.gmail,
+    pass: process.env.gmailPass
+  }
+});
 
-exports.sendWelcomeMail = (user)=>{
-    const data = {
-        from: 'Kiubo? 👻 <fixtergeek@gmail.com>',
-        to: user.email, 
-        subject: 'Probando esta', 
-        text: `Hola ${user.username} Bienvenido a nuestra ironApp`,
-        //html: '<b>Awesome Message</b>'
-      }
-    transporter.sendMail(data)
-      .then(info => console.log(info))
-      .catch(error => console.log(error))
+exports.sendWelcomeMail = (user) => {
+  const data = {
+    from: 'Kiubo? 👻 <patrickdlg14@gmail.com>',
+    to: user.email,
+    subject: 'Probando esta',
+    text: `Hola ${user.username} Bienvenido a nuestra ironApp`,
+    //html: '<b>Awesome Message</b>'
+  }
+  transporter.sendMail(data)
+    .then(info => console.log(info))
+    .catch(error => console.log(error))
 }
 
 const welcomeCompile = hbs.compile(fs.readFileSync((__dirname, './views/welcome.hbs'), 'utf8'));
 
-exports.sendTemplate = (user) => {
-    const data = {
-        from: 'Kiubo? 👻 <fixtergeek@gmail.com>',
-        to: user.email, 
-        subject: 'Probando esta', 
-        //text: `Hola ${user.username} Bienvenido a nuestra ironApp`,
-        html: welcomeCompile(user)
-      }
-    transporter.sendMail(data)
-      .then(info => console.log(info))
-      .catch(error => console.log(error))
-} ;
+exports.sendTemplate = (user) =>  {
+  const data = {
+    from: 'Kiubo? 👻 <patrickdlg14@gmail.com@gmail.com>',
+    to: user.email,
+    subject: 'Probando esta',
+    //text: `Hola ${user.username} Bienvenido a nuestra ironApp`,
+    html: welcomeCompile(user)
+  }
+  transporter.sendMail(data)
+    .then(info => console.log(info))
+    .catch(error => console.log(error))
+};
